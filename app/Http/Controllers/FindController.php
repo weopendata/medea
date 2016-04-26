9718500914beaf107828a8914dbf0ed14834aad8
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2016-04-26 13:59:35 +0200

    Small fix for non logged in users

--- app/Http/Controllers/FindController.php
+++ app/Http/Controllers/FindController.php
@@ -72,8 +72,6 @@ class FindController extends Controller
 
         $query_string = $this->buildQueryString($request);
 
-        \Log::info($query_string);
-
         foreach ($pages as $rel => $page_info) {
             $link_header .= $request->url() . '?offset=' . $page_info[0] . '&limit=' . $page_info[1] . $query_string .';rel=' . $rel . ';';
         }
