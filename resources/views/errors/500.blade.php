ce5bb565cf3adbc140b8934ef6679d1057f8787c
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2016-08-10 08:57:36 +0200

    Issue #39

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Error - MEDEA</title>
+    <style type="text/css">
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        color: #B0BEC5;
+        font-family: lato, open sans, helvetica neue, helvetica, segoe ui, sans-serif;
+    }
+
+    .container {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    }
+
+    h1 {
+        font-size: 72px;
+        font-weight: 100;
+        margin-bottom: 1em
+    }
+
+    p {
+        margin-top: 3em;
+    }
+
+    a {
+        color: #390;
+    }
+
+    .return {
+        display: inline-block;
+        padding: 1em 2em;
+        border: 2px solid #390;
+        font-size: 24px;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-weight: 300;
+        letter-spacing: 2px;
+    }
+
+    .return:hover {
+        outline: 2px solid #390;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <main>
+            <h1>Error</h1>
+            <p>
+                Er is een onverwacht fout opgetreden.
+            </p>
+            <p>
+                <a href="/" class="return">klik hier om terug te keren naar de hoofdpagina</a>
+            </p>
+            <p>
+                Indien deze fout blijft voorkomen contacteer ons: <a href="mailto:pdeckers@vub.ac.be">pdeckers@vub.ac.be</a>
+            </p>
+        </main>
+    </div>
+</body>
+
+</html>
