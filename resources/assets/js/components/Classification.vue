975278de5c928e50b13498021a2bfa5a01d0ec63
Thomas <info@thomasg.be>
2016-08-23 15:32:09 +0200

    Update classification view

--- resources/assets/js/components/Classification.vue
+++ resources/assets/js/components/Classification.vue
@@ -1,46 +1,57 @@
 <template>
-<div class="card card-center fe-card">
+  <div class="card card-center cls-card">
     <div class="card-textual">
-      <p>
-        Deze classificatie werd opgesteld door vondstexpert
-      </p>
-      <div v-if="cls.productionClassificationPeriod || cls.productionClassificationNation">
+      <p v-if="cls.productionClassificationPeriod || cls.productionClassificationNation">
         <span class="cls-labeled" v-if="cls.productionClassificationPeriod">Periode <b>{{cls.productionClassificationPeriod}}</b></span>
         <span class="cls-labeled" v-if="cls.productionClassificationNation">Natie <b>{{cls.productionClassificationNation}}</b></span>
         <span class="cls-labeled" v-if="cls.productionClassificationType">Type <b>{{cls.productionClassificationType}}</b></span>
         <span class="cls-labeled" v-if="cls.startDate||cls.endDate">Datering <b>{{cls.startDate || '?'}} - {{cls.endDate || '?'}}</b></span>
-      </div>
+      </p>
       <p class="cls-p" v-if="cls.productionClassificationDescription" v-text="cls.productionClassificationDescription"></p>
       <p v-if="singlePub">
-        <i class="bookmark icon"></i>
         Referentie:
         <a :href="singlePub.href" v-if="singlePub.href" v-text="singlePub.href"></a>
-        <span v-else v-text="singlePub.text"></span>@@
+        <span v-else v-text="singlePub.text"></span>@
       </p>
-      <div v-if="multiPub">
-        <p v-if="cls.publication&&cls.publication.length">
-          <i class="bookmark icon"></i>
-          Referenties:
-        </p>
-        <div v-for="pub in pubs">@
+      <p v-if="multiPub">
+        Referenties:
+        <span v-for="pub in pubs">
+          <br>
           <a :href="pub.href" v-if="pub.href" v-text="pub.href"></a>
           <span v-else v-text="pub.text"></span>
-        </div>
+        </span>
+      </p>
+      <p v-if="cls.agree">
+        <i class="thumbs up icon"></i> {{cls.agree}} vondstexperts zijn het eens met deze classificatie
+      </p>
+      <p v-if="cls.disagree">
+        <i class="thumbs down icon"></i> {{cls.disagree}} vondstexperts zijn het eens met deze classificatie
+      </p>
+    </div>
+    <div class="card-bar card-bar-border">
+      <span v-dif="user.vondstexpert">
+        <button class="btn" :class="{green:cls.me==='agree'}" @click.stop="agree"><i class="thumbs up icon"></i></button>
+        <button class="btn" :class="{red:cls.me==='disagree'}" @click.stop="disagree"><i class="thumbs down icon"></i></button>
+      </span>{{cls.me}}
+      <span class="cls-creator" v-if="creator||cls.created_at">
+        Opgesteld
+        <span v-if="creator">door {{creator}}</span>
+        <span v-if="cls.created_at">op {{cls.updated_at}}</span>
+        <span v-if="cls.updated_at!==cls.created_at" style="margin-left:1em">update op {{cls.updated_at}}</span>
+      </span>
+      <div style="float:right">
+        <button class="btn" @click.prevent.stop="rm" v-if="$root.user.administrator"><i class="trash icon"></i></button>
       </div>
     </div>
-    <div class="card-bar">
-      <button class="btn" :class="{green:me==='agree'}" @click.stop="agree">{{cls.agree}} <i class="thumbs up icon"></i></button>
-      <button class="btn" :class="{red:me==='disagree'}" @click.stop="disagree">{{cls.disagree}} <i class="thumbs down icon"></i></button>
-
-      <button class="btn" @click.prevent.stop="rm" v-if="$root.user.administrator" style="float:right"><i class="trash icon"></i></button>
-
-    </div><!--v-if-->
   </div>
 </template>
 
 <script>
 function urlify (u) {
-  if (u && u.slice(0, 4) === 'http') {
+  if (!u) {
+    return
+  }
+  if (u.slice(0, 4) === 'http') {
     return {
       href: u
     }
@@ -51,51 +62,42 @@ function urlify (u) {
 }
 export default {
   props: ['cls', 'obj'],
-  data () {
-    return {
-      me: this.cls ? this.cls.me : false
-    }
-  },
   computed: {
+    creator () {
+      return this.cls && this.cls.person && this.cls.person.name
+    },
     multiPub () {
-      return this.cls.publication && this.cls.publication.length > 1
+      return this.pubs && this.pubs.length > 1
     },
     singlePub () {
       return this.pubs && this.pubs.length === 1 && this.pubs[0]
     },
     pubs () {
-      return this.cls.publication && this.cls.publication.map(p => urlify(p.publicationTitle))
+      return this.cls.publication && this.cls.publication.map(p => urlify(p.publicationTitle)).filter(Boolean)
     }
   },
   methods: {
     agree () {
-      this.cls[this.me]--
-      this.me = this.me === 'agree' ? false : 'agree'
-      this.cls[this.me]++
+      this.cls[this.cls.me]--
+      this.$set('cls.me', this.cls.me === 'agree' ? null : 'agree')
+      this.cls[this.cls.me]++
       this.$http({
-        method: this.me ? 'POST' : 'DELETE',
+        method: this.cls.me ? 'POST' : 'DELETE',
         url: '/objects/' + this.obj + '/classifications/' + (this.cls.identifier || -1) + '/agree'
       })
     },
     disagree () {
-      this.cls[this.me]--
-      this.me = this.me === 'disagree' ? false : 'disagree'
-      this.cls[this.me]++
+      this.cls[this.cls.me]--
+      this.$set('cls.me', this.cls.me === 'disagree' ? null : 'disagree')
+      this.cls[this.cls.me]++
       this.$http({
-        method: this.me ? 'POST' : 'DELETE',
+        method: this.cls.me ? 'POST' : 'DELETE',
         url: '/objects/' + this.obj + '/classifications/' + (this.cls.identifier || -1) + '/disagree'
       })
     },
     rm () {
       this.$http.delete('/objects/' + this.obj + '/classifications/' + (this.cls.identifier || -1)).then(this.$root.fetch)
     }
-  },
-  watch: {
-    me (v) {
-      this.cls.me = v
-    }
-  },
-  components: {
   }
 }
 </script>
@@ -106,6 +108,15 @@ export default {
   border-bottom: 1px solid #ccc;
   padding: 1rem 0;
   overflow: auto;
+}
+.cls-card {
+  padding-top: 16px;
+}
+.cls-creator  {
+  padding-left: 1rem;
+  font-size: 12px;
+  line-height: 36px;
+  color: #999;
 }
 .cls-content  {
   margin-left: 100px;
@@ -123,8 +134,7 @@ export default {
 }
 .cls-labeled {
   display: inline-block;
-  margin-right: 1rem;
-  margin-bottom: 1rem;
+  margin: .5rem 1rem .5rem 0;
   padding: 2px 2px 2px 1rem;
   color: #666;
   line-height: 2rem;
@@ -138,6 +148,7 @@ export default {
   background-color: white;
 }
 .cls-p {
+  font-size: 16px;
   padding: 5px 0;
   white-space: pre-wrap;
 }
