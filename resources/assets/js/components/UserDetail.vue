736942b8121c7636476620dd5c117124081759d0
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2019-12-18 17:42:37 +0100

    Clean up commented lines

--- resources/assets/js/components/UserDetail.vue
+++ resources/assets/js/components/UserDetail.vue
@@ -12,52 +12,31 @@
       Lid van MEDEA sinds {{ profile.created_at }}
     </p>
 
-    <!--@if (!empty($profile['expertise']) || !empty($profile['bio']) || !empty($profile['research']) || !empty($profile['function']) || !empty($profile['affiliation']))-->
     <template v-if="profile.expertise || profile.bio || profile.research || profile.function || profile.affiliation">
       <h3>Over mij</h3>
-      <!--@if (!empty($profile['function'])-->
       <template v-if="profile.function">
         <p>
-          <b>Functie</b>: {{profile.function}} <!--{!! nl2br(e($profile['function'])) !!}-->
+          <b>Functie</b>: {{profile.function}}
         </p>
       </template>
-      <!--@endif-->
-      <!--@if (!empty($profile['affiliation']))-->
       <template v-if="profile.affiliation">
         <p>
-          <b>Instelling</b>: {{profile.affiliation}}<!--{!! nl2br(e($profile['affiliation'])) !!}-->
+          <b>Instelling</b>: {{profile.affiliation}}
         </p>
       </template>
-      <!--@endif-->
-      <!--@if (!empty($profile['research']))-->
       <p>
-        <b>Onderzoek</b>: {{profile.research}} <!--{!! nl2br(e($profile['research'])) !!}-->
+        <b>Onderzoek</b>: {{profile.research}}
       </p>
-      <!--@endif-->
-      <!--@if (!empty($profile['bio']))-->
       <p>
-        <b>Bio</b>: {{profile.bio}}<!--{!! nl2br(e($profile['bio'])) !!}-->
+        <b>Bio</b>: {{profile.bio}}
       </p>
-      <!--@endif-->
-      <!--@if (!empty($profile['expertise']))-->
       <p>
-        <b>Expertise</b>: {{profile.expertise}}<!--{!! nl2br(e($profile['expertise'])) !!}-->
+        <b>Expertise</b>: {{profile.expertise}}
       </p>
-      <!--@endif-->
     </template>
-    <!--@endif-->
-  <!--@endif-->
-
-  <!--@if (! empty(session('message')))
-  <div class="ui positive message">
-    <i class="close icon"></i>
-    <p>{!! nl2br(e(session('message'))) !!}</p>
-  </div>
-  @endif-->
 
   <template v-if="user.id != id">
     <h3>Contact</h3>
-    <!--@if (@$profile['showEmail'])-->
     <template v-if="profile.showEmail">
       <p>
         Email: <a :href="'mailto:' + profile.email">{{ profile.email }}</a>
@@ -80,14 +59,12 @@
       </div>
     </template>
   </template>
-  <!--@endif-->
 
   <h3>Rollen:</h3>
   <ul>
     <li v-for="role in roles">{{ role }}</li>
   </ul>
 
-  <!--@if (isset($collections))-->
   <template v-if="collections">
     <div>
       <h3>Collecties:</h3>
@@ -101,7 +78,6 @@
       </user-collections>
     </div>
   </template>
-  <!--@endif-->
   </div>
 </template>
 
